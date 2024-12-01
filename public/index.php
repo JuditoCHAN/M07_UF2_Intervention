fc65c6c2530b5c45e6c598c4e4b3c79aec2c1595
judith <judith.bello.torralbo@gmail.com>
2024-12-01 18:33:56 +0100

    Solucionado el issue #2. En el option del select ponía sharpem en vez de sharpen

--- public/index.php
+++ public/index.php
@@ -11,7 +11,7 @@
             <option value="blur" selected>Blur</option>
             <option value="bright" selected>Bright</option>
             <option value="fit">Fit</option>
-            <option value="sharpem">Sharpen</option>
+            <option value="sharpen">Sharpen</option>
         </select>
         <br>
         <h3>Choose grade</h3>
